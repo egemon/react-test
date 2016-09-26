772d06bb0cb30341ffff6c66e9e4c8fa0d1d598a
ilukianov <ilukianov@cogniance.com>
2016-09-26 17:35:58 +0300

    Refactored with reducer composition

--- Store/store.js
+++ Store/store.js
@@ -3,25 +3,38 @@
  */
 import config from "../config";
 import { createStore } from "redux";
-const counter = (state = JSON.parse(JSON.stringify(config.GAME)), action) => {
+const changeField = (object, {fieldName, fieldValue}) => {
+  return {
+    ...object,
+    [fieldName]: fieldValue
+  };
+};
+
+const metadata = (state = {...config.GAME.metadata}, action) => {
+  switch (action.type) {
+  case "METADATA_CHANGE":
+    state = changeField(state, action);
+  }
+  return state;
+};
+
+const players = (state = (new Array(config.PLAYER_NUMBER)).fill(), action) => {
   switch (action.type) {
   case "PLAYER_CHANGE":
-    state.players[action.number] = {
-      ...state.players[action.number],
-      [action.fieldName]: action.fieldValue
-    };
-    return state;
-  case "METADATA_CHANGE":
-    state.metadata = {
-      ...state.metadata,
-      [action.fieldName]: action.fieldValue
-    };
-    return state;
+    state[action.number] = changeField(state[action.number], action);
+  }
+  return state;
+};
+
+const counter = (state = JSON.parse(JSON.stringify(config.GAME)), action) => {
+  switch (action.type) {
   case "CLEAR":
     return JSON.parse(JSON.stringify(config.GAME));
-  default:
-    return state;
   }
+  return {
+    metadata: metadata(state.metadata, action),
+    players: players(state.players, action),
+  };
 };
 const store = createStore(counter);
 
